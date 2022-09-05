aa36bdd55a37474e3eaad3c2a1f1490c3ae1fec5
KamilKorczek <korczek@gmail.com>
2022-09-05 22:10:22 +0200

    Get href attribute from the clicked link, next find the correct article using the selector and add class active to the correct article in the scrypt.js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
+  console.log('clickedElement (with plus): ' + clickedElement);
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -11,7 +12,6 @@ function titleClickHandler(event){
   activeLink.classList.remove('active');
   }
   /* [DONE] add class 'active' to the clicked link */
-  console.log('clickedElement (with plus): ' + clickedElement);
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
@@ -20,11 +20,16 @@ function titleClickHandler(event){
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add("active");
 }
 
 const links = document.querySelectorAll('.titles a');
